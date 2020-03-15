7ddbe506cd483dfd097737625df4566a5462d5e6
mengkunsoft <1163540807@qq.com>
2020-03-15 15:02:08 +0800

    开坑！

---
+++ README.md
@@ -0,0 +1,28 @@
+# WordPress 主题 Light Year
+
+#### 说明
+
+移植自 @笔下光年 的静态博客页面模板 Light Year Blog，源文件地址： https://gitee.com/yinqi/Light-Year-Blog
+
+目前尚未完工。
+
+---
+
+#### 演示地址
+
+[http://libs.itshubao.com/blog/](http://libs.itshubao.com/blog/)
+
+#### 介绍
+
+个人非常喜欢左右分栏的这种布局，加上左侧的背景图每天更换，让人有不一样的心情，从最初的到现在，大概这个是第四个版本的布局模板了，除了最早的一个模板，其他的也都分享出来了，这一个是最新的一个，属于博客类型的模板。
+
+Light Year Blog是基于Bootstrap v4.3.1的小清新风格的HTML博客模板，只有三个页面，首页，详细页和About页面，样式和js都不多，比较简单。
+
+1. 左右分栏布局，自己搭配左侧背景。
+1. 基于 Bootstrap v4.3.1 编写，响应式布局（台式机、平板电脑、移动设备），支持主流浏览器（Chrome、Firefox、Safari、IE11、Edge）。
+1. 界面设计简洁、清新。
+
+
+#### 截图
+![首页](https://images.gitee.com/uploads/images/2019/1103/214027_5dc5b629_82992.jpeg "未命名-1.jpg")
+![详细页](https://images.gitee.com/uploads/images/2019/1103/214045_d7f79cef_82992.jpeg "笔下光年的博客.jpg")
---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="author" content="yinqi,mengkun" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/materialdesignicons.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/style.min.css" />
+    
+    <?php wp_head(); ?>
+    
+    <script>
+    </script>
+</head>
+<body>
+
+<header class="lyear-header text-center" style="background-image:url(<?php bloginfo('template_url'); ?>/images/left-bg.jpg);">
+    <div class="lyear-header-container">
+        <div class="lyear-mask"></div>
+        <h1 class="lyear-blogger pt-lg-4 mb-0"><a href="index.html">笔下光年的博客</a></h1>
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="lyear-hamburger">
+                    <div class="hamburger-inner"></div>
+                </div>
+            </a>
+            
+            <div id="navigation" class="collapse navbar-collapse flex-column">
+                <div class="profile-section pt-3 pt-lg-0">
+                    <img class="profile-image mb-3 rounded-circle mx-auto" src="<?php bloginfo('template_url'); ?>/images/lyear.png" width="120" height="120" alt="笔下光年" >
+                    <div class="lyear-sentence mb-3">必须记住我们学习的时间是有限的。时间有限，不只由于人生短促，更由于人事纷繁。我们就应力求把我们所有的时间用去做最有益的事情。</div>
+                    <hr>
+                </div>
+                
+                <ul class="navbar-nav flex-column text-center">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.html">首页</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">技术</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">感悟</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">关于我</a>
+                    </li>
+                </ul>
+                
+                <div class="my-2 my-md-3">
+                    <form class="lyear-search-form form-inline justify-content-center pt-3">
+                        <input type="email" id="semail" name="semail1" class="form-control mr-md-1" placeholder="搜索关键词" />
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
